0ecbce1eaa93dbdcb4bb833b08e049a71799e728
garywill <32130780+garywill@users.noreply.github.com>
2017-12-01 11:46:33 +0800

    add chinese traditional translation

---
+++ lang/chinese_big5.php
@@ -0,0 +1,167 @@
+<?php 
+# General
+# 翻譯由森林之原提供：http://openlyu.com/
+# capriskye::andrew@openlyu.com
+// Last updated: Tue, 12 December 2006 18:24:26 GMT
+
+$LANG['metatag']    = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+
+$LANG['Search']      =  '搜尋';
+$LANG['SearchMess']  =  '訊息';
+
+$LANG['BookMess1']  = 'HTML 代碼關閉';
+$LANG['BookMess2']  = 'HTML 代碼開啟';
+$LANG['BookMess3']  = '留言';
+$LANG['BookMess4']  = '回到留言版';
+$LANG['BookMess5']  = '管理';
+$LANG['BookMess6']  = '感謝您的光臨。請留下您的訊息。';
+$LANG['BookMess7']  = '留言';
+$LANG['BookMess8']  = '回到送出表格';
+$LANG['BookMess9']  = '留下訊息到此留言。';
+$LANG['BookMess10'] = '<u>謝謝您的留言。</u><br /><br />您的留言已經完成！轉址中請稍待...';
+$LANG['BookMess11'] = '<u>謝謝您的留言。</u><br /><br />管理員必須先審核您的留言，他們已經被通知。';
+$LANG['BookMess12'] = 'Show Comments';
+$LANG['BookMess13'] = 'Hide Comments';
+
+# Email Subjects
+$LANG['EmailAdminSubject']    =  '新留言';
+$LANG['EmailAdminComSubject'] =  '新留言';
+$LANG['EmailGuestSubject']    =  '謝謝您的留言';
+
+# Admin 
+$LANG['AdminAccept']          =  '接受';
+$LANG['AdminUnaccept']        =  '不接受';
+$LANG['AdminEdit']            =  '編輯';
+$LANG['AdminDelete']          =  '刪除';
+$LANG['AdminPicDelete']       =  '刪除圖片';
+$LANG['AdminBanIP']           =  '禁止 IP';
+$LANG['AdminBanIPDel']        =  '禁止 IP 及刪除';
+$LANG['AdminRemember']        =  '記得我';
+$LANG['AdminJSMultiAccept']   =  '確定要接受所選的留言嗎？';
+$LANG['AdminJSMultiUnaccept'] =  '確定不接受所選的留言嗎？';
+$LANG['AdminJSMultiDelete']   =  '確定要刪除所選的留言嗎？';
+$LANG['AdminJSNoAction']      =  '沒有完成任何動作';
+$LANG['AdminJSBanDel']        =  '確定要刪除此留言及禁止嗎';
+$LANG['AdminJSBanIP']         =  '確定要禁止嗎';
+$LANG['AdminJSPicDelete']     =  '確定要刪除此圖片嗎？';
+$LANG['AdminJSDelete']        =  '確定要刪除此留言嗎？';
+$LANG['AdminJSInvalidRec']    =  '錯誤的資料號碼！';
+$LANG['AdminTopMesssDel']     =  '留言已刪除。';
+$LANG['AdminTopMessDel']      =  '留言已刪除。';
+$LANG['AdminTopImageDel']     =  '圖片已刪除。';
+$LANG['AdminTopMessAcc']      =  '留言已接受。';
+$LANG['AdminTopMesssAcc']     =  '留言已接受。';
+$LANG['AdminTopMessUnacc']    =  '不接受留言。';
+$LANG['AdminTopMesssUnacc']   =  '不接受留言。';
+$LANG['AdminTopIPBanned']     =  'IP 已禁止。';
+
+$LANG['EmailMess1'] = '被留言的項目';
+$LANG['EmailMess2'] = '留言';
+
+# Just for the search
+$LANG['NoResults']   =  '抱歉但找不到任何資料。';
+$LANG['SearchFor']   =  '給';
+
+# Alternate image text
+$LANG['AltIP']    = 'IP 已紀錄';
+$LANG['AltICQ']   = '顯示 ICQ 資料';
+$LANG['AltEmail'] = '寄電子郵件';
+$LANG['AltUrl']   = '瀏覽網頁';
+$LANG['AltAim']   = '寄 AIM 訊息';
+$LANG['AltCom']   = '留言';
+$LANG['AltYahoo'] = '顯示 Yahoo 資料';
+$LANG['AltMSN']   = '顯示 MSN 資料';
+$LANG['AltSkype'] = '用 Skype 打電話';
+
+# Form
+$LANG['FormMess1']   = '請輸入您的資料和訊息。留言送出之後您會被轉址回到留言版。有 (*) 表示必須填的欄位。';
+$LANG['FormMess2']   = '表情圖案開啟';
+$LANG['FormMess3']   = 'AGCode 開啟';
+$LANG['FormMess4']   = '顯示全部的表情圖案';
+$LANG['FormMess5']   = '下面是您的留言。';
+$LANG['FormMess6']   = 'AG 關閉';
+$LANG['FormMess7']   = 'Smilies 關閉';
+$LANG['FormName']    = '名字';
+$LANG['FormEmail']   = '電子郵件';
+$LANG['FormLoc']     = '位址';
+$LANG['FormPic']     = '圖片';
+$LANG['FormUrl']     = '個人網頁';
+$LANG['FormGender']  = '性別';
+$LANG['FormMale']    = '男';
+$LANG['FormFemale']  = '女';
+$LANG['FormNeuter']	= '不顯示';
+$LANG['FormMessage'] = '您的訊息';
+$LANG['FormSelect']  = '留言版';
+$LANG['FormUser']    = '帳號';
+$LANG['FormPass']    = '密碼';
+$LANG['FormReset']   = '重設';
+$LANG['FormSubmit']  = '送出';
+$LANG['FormPreview'] = '預覽';
+$LANG['FormBack']    = '返回';
+$LANG['FormEnter']   = '留言版 - 管理中心<br />繼續前您必須提供帳號跟密碼！';
+$LANG['FormButton']  = '繼續';
+$LANG['FormPriv']    = '個人訊息';
+$LANG['FormMSNeg']	= '<br />MSN 帳號範例：<br />name_123@hotmail.com myname@msn.com example@passport.com';
+$LANG['FormAGcode']	= '<b>允許的 AGcodes 有：</b>';
+$LANG['FormHTMcode'] = '<b>允許的 HTML 標示有：</b>';
+$LANG['FormBot'] 		= '反機器垃圾測試';
+$LANG['FormCAPTCHA'] = '請輸入圖片裡的文字<br>';
+
+# Navigation Bar
+$LANG['NavTotal']   = '總留言：';
+$LANG['NavRecords'] = '每頁的留言數：';
+$LANG['NavPrev']    = '上一頁';
+$LANG['NavNext']    = '下一頁';
+
+# Post Errors
+$LANG['ErrorPost1']  = '您忘了輸入名字，請輸入。';
+$LANG['ErrorPost2']  = '您忘了輸入訊息，請輸入。';
+$LANG['ErrorPost3']  = '您的留言太短或太長，請重新輸入。';
+$LANG['ErrorPost4']  = '其中一個欄位有錯誤。';
+$LANG['ErrorPost5']  = '留言版 - 錯誤';
+$LANG['ErrorPost6']  = '圖片檔名太長';
+$LANG['ErrorPost7']  = '圖片類型錯誤';
+$LANG['ErrorPost8']  = '抱歉！我們使用了灌水的限制。請稍待在試。';
+$LANG['ErrorPost9']  = '您的 IP 已被禁止！';
+$LANG['ErrorPost10'] = '您的留言包含了錯誤的文字，請修改。';
+$LANG['ErrorPost11'] = '您忘了輸入留言，請輸入。';
+$LANG['ErrorPost12'] = '您忘了輸入電子郵件，請輸入。';
+$LANG['ErrorPost13'] = '您忘了輸入反機器垃圾的文字，請輸入。';
+$LANG['ErrorPost14'] = '您輸入了錯誤的反機器垃圾文字，請重新輸入。';
+$LANG['ErrorPost15'] = '抱歉，您太快留言了。';
+$LANG['ErrorPost16'] = '抱歉，您太晚送出留言了，工作程序已過期。';
+
+$LANG['PassMess1'] = '請輸入正確的帳號跟密碼：';
+$LANG['PassMess2'] = '錯誤的帳號跟密碼，請重試。';
+$LANG['PassMess3'] = '抱歉，您輸入的密碼錯誤。';
+
+# Times
+$times['seconds'] = '%T 秒以前';
+$times['minutes'] = '%T 分以前';
+$times['today'] = '今天於 %T';
+$times['yesterday'] = '昨天於 %T';
+
+# Days
+$weekday[0] = '星期天';
+$weekday[1] = '星期一';
+$weekday[2] = '星期二';
+$weekday[3] = '星期三';
+$weekday[4] = '星期四';
+$weekday[5] = '星期五';
+$weekday[6] = '星期六';
+
+# Months
+$months[0]  = '一月';
+$months[1]  = '二月';
+$months[2]  = '三月';
+$months[3]  = '四月';
+$months[4]  = '五月';
+$months[5]  = '六月';
+$months[6]  = '七月';
+$months[7]  = '八月';
+$months[8]  = '九月';
+$months[9]  = '十月';
+$months[10] = '十一月';
+$months[11] = '十二月';
+
+?>
